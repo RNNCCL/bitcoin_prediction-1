be9282d5801ddee0db9242e02656f4ed3c00ccd1
tushar garg <tushargarg@email.arizona.edu>
2017-06-12 18:00:37 -0700

    basic code change

--- access_api.py
+++ access_api.py
@@ -1,1 +1,12 @@
-print("hello world")
+import json
+import urllib2
+import requests
+
+url = "https://blockchain.info/ticker"
+
+# this takes a python object and dumps it to a string which is a JSON
+# representation of that object
+data = json.load(urllib2.urlopen(url))
+
+# print the result
+print (data)
